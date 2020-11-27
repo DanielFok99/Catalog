5a8872685358338cf9b5573f126ad058f0651018
DanielFok99 <43980351+DanielFok99@users.noreply.github.com>
2020-11-28 02:14:47 +0800

    Last commit

--- insert_book.php
+++ insert_book.php
@@ -17,40 +17,58 @@ $title = '';
 
 // TODO 2: Check and filter data coming from the user.
 
-if (isset($_POST['isbn']) && isset($_POST['author']) && isset($_POST['title']) && isset($_POST['price'])) {
-    echo 'test';
-
-    $isbn = htmlspecialchars($_POST['isbn']);
-    $author = htmlspecialchars($_POST['author']);
-    $title = htmlspecialchars($_POST['title']);
-    $price = htmlspecialchars($_POST['price']);
-
-
-}
+if (
+    isset($_POST['isbn']) && !empty(trim($_POST['isbn'])) &&
+    isset($_POST['author']) && !empty(trim($_POST['author'])) &&
+    isset($_POST['title']) && !empty(trim($_POST['title'])) &&
+    isset($_POST['price']) && !empty(trim($_POST['price']))
+) {
 
 // TODO 3: Setup a connection to the appropriate database.
+    $isbn = htmlspecialchars(trim($_POST['isbn']));
+    $author = htmlspecialchars(trim($_POST['author']));
+    $title = htmlspecialchars(trim($_POST['title']));
+    $price = htmlspecialchars(trim($_POST['price']));
 
-
-$conn = mysqli_connect('localhost', 'root', '', 'publications');
-if ($conn->connect_error) {
-    die('Connection failed' . $conn->connect_error);
-}
+    if (is_numeric($price) && is_numeric($isbn) && (strlen($isbn) == 10 || strlen($isbn) == 13)) {
+        $conn = mysqli_connect('localhost', 'root', '', 'publications');
+        if ($conn->connect_error) {
+            die('Connection failed' . $conn->connect_error);
+        }
 
 // TODO 4: Query the database.
 
 
-$result = $conn->query("INSERT INTO catalogs(isbn,author,title,price) VALUES ('$isbn','$author','$title','$price')") or die($conn->error);
+        $result = $conn->query("INSERT INTO catalogs(isbn,author,title,price) VALUES ('$isbn','$author','$title','$price')") or die($conn->error);
 
 
 // TODO 5: Display the feedback back to user.
 
-if ($result) {
-    echo '<p>Book has insert successfully</p>';
-}
+        if ($result) {
+            echo '<p>Book has insert successfully</p>';
+        }
 
 // TODO 6: Disconnecting from the database.
-$conn->close();
+        $conn->close();
+    } else {
 
+        if (!is_numeric($isbn)) {
+            echo '<p>Insert failed, Please provide a numeric ISBN</p>';
+        } else {
+            if (strlen($isbn) != 10 && strlen($isbn) != 13) {
+                echo '<p>Insert failed, Please provide a formal ISBN</p>';
+            }
+        }
+
+        if (!is_numeric($price)) {
+            echo '<p>Insert failed, Please provide a numeric price</p>';
+        }
+
+    }
+
+} else {
+    echo '<p>Insert failed, Please provide require information</p>';
+}
 ?>
 </body>
 </html>
--- results.php
+++ results.php
@@ -4,33 +4,101 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Book Search Results</title>
+    <style>
+        table {
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        td, th {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+        }
+
+        tr:nth-child(even) {
+            background-color: #dddddd;
+        }
+    </style>
 </head>
 <body>
-    <h1>Book Search Results</h1>
-    <?php
-    // TODO 1: Create short variable names.
-    $author = '';
-    $title = '';
-    $isbn = '';
+<h1>Book Search Results</h1>
+<?php
+// TODO 1: Create short variable names.
+$searchType = '';
+$searchTerm = '';
 
-    // TODO 2: Check and filter data coming from the user.
 
+// TODO 2: Check and filter data coming from the user.
+
+if (
+    isset($_POST['searchtype']) && !empty(trim($_POST['searchtype'])) &&
+    isset($_POST['searchterm']) && !empty(trim($_POST['searchterm']))
+) {
+
+    switch (htmlspecialchars(trim($_POST['searchtype']))) {
+        case 'author':
+            $searchType = 'author';
+            break;
+        case 'title':
+            $searchType = 'title';
+            break;
+        case 'isbn':
+            $searchType = 'isbn';
+            break;
+    }
+
+    $searchTerm = htmlspecialchars(trim($_POST['searchterm']));
 
     // TODO 3: Setup a connection to the appropriate database.
-
+    $conn = mysqli_connect('localhost', 'root', '', 'publications');
+    if ($conn->connect_error) {
+        die('Connection failed' . $conn->connect_error);
+    }
 
     // TODO 4: Query the database.
 
+    $query = "SELECT * FROM catalogs WHERE $searchType LIKE '%$searchTerm%'";
+    $result = $conn->query($query);
 
     // TODO 5: Retrieve the results.
-
-
     // TODO 6: Display the results back to user.
+
+    if ($result->num_rows > 0) {
+        echo '<table>
+                <thead>
+                <tr>
+                    <th scope="col">ISBN</th>
+                    <th scope="col">Author</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Price</th>
+                </tr>
+                </thead>
+                <tbody>';
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr>";
+            echo '<td>' . $row['isbn'] . '</td>';
+            echo '<td>' . $row['author'] . '</td>';
+            echo '<td>' . $row['title'] . '</td>';
+            echo '<td>' . $row['price'] . '</td>';
+            echo "</tr>";
+        }
+        echo '  </tbody>
+              </table>';
+    } else {
+        echo 'No result';
+    }
+
+
+    $conn->close();
 
 
     // TODO 7: Disconnecting from the database.
 
-
-    ?>
+} else {
+    echo '<p>Search failed, Please provide require information</p>';
+}
+?>
 </body>
 </html>
